1c106d38c0b46d94673b8db8762fc691f803510f
Kevin Wang <kevin@xorkevin.com>
2019-05-15 21:31:59 -0700

    Add knuth linebreak implementation

--- linebreak/linebreak.go
+++ linebreak/linebreak.go
@@ -18,16 +18,63 @@ func splitWords(paragraph string) ([]string, []int) {
 
 type (
 	breakpoint struct {
-		currcost  int
-		prevcost  int
-		prevbreak int
+		cost int
+		prev int
 	}
 )
 
+func squareCost(x, w int) int {
+	k := w - x
+	return k * k
+}
+
+func lineCost(x, w int, last bool) int {
+	if last {
+		return 0
+	}
+	return squareCost(x, w)
+}
+
 // findBreakpointsKnuth takes in a list of word lengths and maximum width, and
 // finds the optimal breakpoints minimizing the squares of line end spaces.
-func findBreakpointsKnuth(wordLengths []int, width int) []int {
-	return []int{}
+func findBreakpointsKnuth(wordLengths []int, width int) (int, []int) {
+	last := len(wordLengths)
+	dpCache := make([]breakpoint, 0, len(wordLengths)+1)
+	dpCache = append(dpCache, breakpoint{
+		cost: 0,
+		prev: 0,
+	})
+	for i := range wordLengths {
+		accWidth := wordLengths[i]
+		minCost := lineCost(accWidth, width, i == last-1) + dpCache[i].cost
+		minPrev := i
+		for j := i - 1; j >= 0; j-- {
+			accWidth += wordLengths[j] + 1
+			if accWidth > width {
+				break
+			}
+			c := lineCost(accWidth, width, i == last-1) + dpCache[j].cost
+			if c < minCost {
+				minCost = c
+				minPrev = j
+			}
+		}
+		dpCache = append(dpCache, breakpoint{
+			cost: minCost,
+			prev: minPrev,
+		})
+	}
+
+	finalcost := dpCache[last].cost
+	breakstack := []int{}
+	for i := dpCache[last].prev; i > 0; i = dpCache[i].prev {
+		breakstack = append(breakstack, i)
+	}
+	for i := 0; i < len(breakstack)/2; i++ {
+		j := len(breakstack) - i - 1
+		breakstack[i], breakstack[j] = breakstack[j], breakstack[i]
+	}
+	return finalcost, breakstack
 }
 
 // WrapParagraphs wraps stdin or an input file on a per paragraph basis. A
--- linebreak/linebreak_test.go
+++ linebreak/linebreak_test.go
@@ -13,3 +13,11 @@ func Test_splitWords(t *testing.T) {
 	assert.Equal([]string{"Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit."}, words, "words should split on any space character")
 	assert.Equal([]int{5, 5, 5, 3, 5, 11, 10, 5}, wordLengths, "word lengths should match their corresponding word lengths")
 }
+
+func Test_findBreakpointsKnuth(t *testing.T) {
+	assert := assert.New(t)
+
+	cost, breakstack := findBreakpointsKnuth([]int{3, 2, 2, 5}, 6)
+	assert.Equal(10, cost, "cost should be sum of squares discounting the last line")
+	assert.Equal([]int{1, 3}, breakstack, "breakstack should not be greedy")
+}
